6d69dd895f071057282e9d17b558ac49e4715dda
Allan Schroer <allanschroer@outlook.com>
2021-10-09 19:43:26 -0300

    Add project files.

---
+++ Aplicacao-Transferencia-Bancaria-DIO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aplicacao_Transferencia_Bancaria_DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
